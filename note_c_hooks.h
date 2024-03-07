5811fbe8ca02dcaea07c38374a32a15285412989
Alex Bucknall <alex.bucknall@gmail.com>
2024-03-07 13:25:05 +0000

    Adjusted Kconfig options to make i2c and serial mutually exclusive
    Change-type: minor
    Signed-off-by: Alex Bucknall <alex.bucknall@gmail.com>

--- note_c_hooks.h
+++ note_c_hooks.h
@@ -8,6 +8,11 @@
 void platform_delay(uint32_t ms);
 uint32_t platform_millis(void);
 
+bool note_serial_reset();
+void note_serial_transmit(uint8_t *text, size_t len, bool flush);
+bool note_serial_available();
+char note_serial_receive();
+
 const char *note_i2c_receive(uint16_t device_address_, uint8_t *buffer_, uint16_t size_, uint32_t *available_);
 bool note_i2c_reset(uint16_t device_address_);
 const char *note_i2c_transmit(uint16_t device_address_, uint8_t *buffer_, uint16_t size_);
